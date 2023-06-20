29241412ddd6f6744d1008d2b04a620639254750
kaito.uchida <kaito.uchida@stv-tech.co.jp>
2023-06-20 22:42:58 +0900

    ScreenUtil導入

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:calendar_app_remake/View/calendar_page.dart';
 import 'package:calendar_app_remake/routing/routing.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:month_year_picker/month_year_picker.dart';
 
@@ -14,26 +15,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        MonthYearPickerLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('ja'),
-      ],
-      debugShowCheckedModeBanner: false,
-      title: '自作カレンダー',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      onGenerateRoute: RouteGenerator.generatedRoute,
-      home: const CalendarPage(),
-    );
+    return ScreenUtilInit(
+        designSize: const Size(375, 660),
+        builder: (context, child) {
+          return MaterialApp(
+            localizationsDelegates: const [
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              MonthYearPickerLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: const [
+              Locale('ja'),
+            ],
+            debugShowCheckedModeBanner: false,
+            title: '自作カレンダー',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            onGenerateRoute: RouteGenerator.generatedRoute,
+            home: const CalendarPage(),
+          );
+        });
   }
 }
